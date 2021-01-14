7ad4b0c5e134887d1274bdf76567235bc8c26705
ivgtr <gear.1112t.o@gmail.com>
2021-01-14 19:29:18 +0900

    :ambulance: add: mutationの動作確認、[]で囲うべし

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,22 +1,33 @@
 import React, { FC } from 'react'
 
-import { useUsersQuery } from '../gen/schema'
+import { useUsersQuery, useDeleteUserMutation } from '../gen/schema'
 
 const Home: FC = () => {
   const usersQuery = useUsersQuery()
+  const [deleteUserMutation] = useDeleteUserMutation()
   return (
     <>
-      {!usersQuery.loading && (
+      {!usersQuery.loading ? (
         <ul>
           {usersQuery?.data?.users &&
             usersQuery.data.users.map((user) => {
               return (
                 <li key={user.id}>
                   {user.id}:{user.name}
+                  <button
+                    onClick={async (e) => {
+                      e.preventDefault()
+                      const { data } = await deleteUserMutation({ variables: { id: user.name } })
+                      console.log(data)
+                      usersQuery.refetch()
+                    }}
+                  ></button>
                 </li>
               )
             })}
         </ul>
+      ) : (
+        <p>loadなう</p>
       )}
     </>
   )
